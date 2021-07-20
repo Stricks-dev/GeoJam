458866ff1430836c4911b532a690c99760fa6417
wazabanda <65979616+wazabanda@users.noreply.github.com>
2021-07-20 09:26:12 +0200

    I fixed my issues

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    Transform playerTransform;
+
+    public float smoothingLook = 5f;
+
+    public Vector3 startOffset;
+    private void Start()
+    {
+        playerTransform = player.transform;
+        startOffset = transform.position - playerTransform.position;
+    }
+    private void Update()
+    {
+        playerTransform = player.transform;
+        transform.position = playerTransform.position + startOffset;
+    }
+}
