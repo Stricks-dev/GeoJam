8c5022d5c68fff8e2fd34ff1defd9213e9c1caee
wazabanda <65979616+wazabanda@users.noreply.github.com>
2021-07-20 10:11:17 +0200

    menu functioning

---
+++ Assets/myChanges/globelStuff.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class globelStuff : MonoBehaviour
+{
+    
+    public Animator DeathScreen;
+    public static globelStuff instance;
+    public int menuIndex;
+    public bool isPaused;
+    public GameObject PauseMenu;
+    private void Awake()
+    {
+        instance = this;
+        if (PauseMenu != null)
+        {
+            PauseMenu.SetActive(false);
+        }
+    }
+    private void Start()
+    {
+        isPaused = false;
+    }
+    private void Update()
+    {
+        if (SceneManager.GetActiveScene().buildIndex != menuIndex)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (isPaused)
+                {
+                    Time.timeScale = 1f;
+                    PauseMenu.SetActive(false);
+                    isPaused = false;
+                }
+                else
+                {
+                    isPaused = true;
+                    PauseMenu.SetActive(true);
+                    Time.timeScale = 0f;
+                }
+            }
+        }
+    }
+    private void Instance_onDeath(object sender, System.EventArgs e)
+    {
+        
+        DeathScreen.SetTrigger("ON");
+    }
+    public void loadScene(int index)
+    {
+        SceneManager.LoadSceneAsync(index);
+    }
+    public void loadMainMenu()
+    {
+        SceneManager.LoadSceneAsync(menuIndex);
+    }
+    public void Reload()
+    {
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void openURL(string URL)
+    {
+        Application.OpenURL(URL);
+    }
+}
---
+++ Assets/myChanges/settings.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class settings : MonoBehaviour
+{
+    [Header("Music")]
+    public AudioMixer audioMixer;
+    public Slider musicVolumeSlider, sfxVolumeSlider;
+    public float max, min;
+    [Header("post-processing")]
+    public GameObject postprocessing;
+    public Toggle Toggle;
+    private void Awake()
+    {
+
+    }
+    private void Start()
+    {
+        if (musicVolumeSlider != null && sfxVolumeSlider != null && audioMixer != null && Toggle != null)
+        {
+            musicVolumeSlider.maxValue = max;
+            musicVolumeSlider.minValue = min;
+
+            sfxVolumeSlider.maxValue = max;
+            sfxVolumeSlider.minValue = min;
+
+            musicVolumeSlider.value = PlayerPrefs.GetFloat("musicVolume", 0);
+            sfxVolumeSlider.value = PlayerPrefs.GetFloat("sfxVolume", 0);
+
+            audioMixer.SetFloat("Music_Volume", musicVolumeSlider.value);
+            audioMixer.SetFloat("Sfx_Volume", sfxVolumeSlider.value);
+
+            if (PlayerPrefs.GetInt("enable-PP", 1) == 1)
+            {
+                Toggle.isOn = true;
+                postprocessing.SetActive(true);
+            }
+            else
+            {
+                Toggle.isOn = false;
+                postprocessing.SetActive(false);
+            }
+        }
+    }
+    public void setMusicVolume(float Volume)
+    {
+        PlayerPrefs.SetFloat("musicVolume", Volume);
+        audioMixer.SetFloat("Music_Volume", Volume);
+        PlayerPrefs.Save();
+    }
+    public void setSFXVolume(float volume)
+    {
+        PlayerPrefs.SetFloat("sfxVolume", volume);
+        audioMixer.SetFloat("Sfx_Volume", volume);
+        PlayerPrefs.Save();
+    }
+    public void setGraphics(float volume)
+    {
+        return;
+    }
+    public void disablePostProcessing(bool toggle)
+    {
+        if (toggle)
+        {
+            PlayerPrefs.SetInt("enable-PP", 1);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("enable-PP", 0);
+        }
+        PlayerPrefs.Save();
+    }
+}
